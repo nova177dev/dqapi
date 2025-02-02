85aaf4a9616608fd55380c5bed27219043983705
nova177rus <nova177rus@gmail.com>
2025-02-02 16:03:01 +0400

    Repo setup related changes

--- dqapidb/Security/Roles/Database Roles/api_user.sql
+++ dqapidb/Security/Roles/Database Roles/api_user.sql
@@ -1,10 +1,10 @@
 ﻿create role api_user
 go
+alter role [api_user] add member dqapi_app;
+go
 alter role db_datareader add member api_user;
 go
 alter role db_datawriter add member api_user;
-go
-alter role [api_user] add member dqapi_app;
 go
 alter role [db_datareader] add member [dqapi_app];
 go
